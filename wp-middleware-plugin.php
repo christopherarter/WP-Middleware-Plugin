5ac997637dd3f981ec57bb97ad7c2e0258f275df
Chris Arter <carter@AdministorsMBP3.fios-router.home>
2019-05-22 17:27:42 -0400

    add version

--- wp-middleware-plugin.php
+++ wp-middleware-plugin.php
@@ -4,6 +4,7 @@
  * Plugin Name: WP Middleware Plugin
  * Description: A middleware helper for the Wordpress REST API.
  * Author: Christopher Arter <christopherarter@gmail.com>
+ * Version: 0.1
  */
 
 require __DIR__ . '/vendor/autoload.php';
